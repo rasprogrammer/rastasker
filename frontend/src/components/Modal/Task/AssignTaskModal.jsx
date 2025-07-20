e8eff9b2dd14b85306459b07d0333e1f34bc7469
rasprogrammer <rasprogrammer@gmail.com>
2025-07-20 14:28:08 +0530

    adding assign task modal ui in task module

--- frontend/src/components/Modal/Task/AssignTaskModal.jsx
+++ frontend/src/components/Modal/Task/AssignTaskModal.jsx
@@ -2,10 +2,27 @@ import PrimaryButton from "@/components/Button/PrimaryButton";
 import SecondaryButton from "@/components/Button/SecondaryButton";
 
 import SearchBar from "@/components/Input/SearchBar";
+import SelectField from "@/components/Input/SelectField";
 import AssignTasksMemberTable from "@/components/Table/AssignTasksMemberTable";
 
 export default function AssignTaskModal({ isOpen, onClose }) {
   if (!isOpen) return null;
+
+  const teams = [
+    { key: "", value: "Select Team" },
+    { key: "1", value: "Team 1" },
+    { key: "2", value: "Team 2" },
+    { key: "3", value: "Team 3" },
+    { key: "4", value: "Team 4" },
+  ];
+
+  const members = [
+    { key: "", value: "Select Member" },
+    { key: "1", value: "Member 1" },
+    { key: "2", value: "Member 2" },
+    { key: "3", value: "Member 3" },
+    { key: "4", value: "Member 4" },
+  ];
 
   return (
     <>
@@ -42,12 +59,35 @@ export default function AssignTaskModal({ isOpen, onClose }) {
             </div>
 
             <div className="p-4 md:p-5 space-y-4">
-              <div className="flex items-center mx-auto gap-2">
-                <SearchBar placeholder="Search tasks"></SearchBar>
-                <PrimaryButton>Assign</PrimaryButton>
-              </div>
-            </div>
 
+              <div className="mb-6">
+                <label htmlFor="">Assigned Member : </label>
+                <input type="text" className="text-center w-full p-2 bg-gray-100" value={"Member 1"} />
+              </div>
+
+              <div className="mb-6">
+                <SelectField
+                  label="Select Team"
+                  type={"text"}
+                  id={"team"}
+                  options={teams}
+                />
+              </div>
+
+              <div className="mb-6">
+                <SelectField
+                  label="Select Team"
+                  type={"text"}
+                  id={"team"}
+                  options={members}
+                />
+              </div>
+              
+              <div className="mb-6">
+                <PrimaryButton>Save</PrimaryButton>
+              </div>
+
+            </div>
           </div>
         </div>
       </div>
